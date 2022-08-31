8fceb449f89288f885903a2941a15190e81b7ee0
Zherebko Dmitry <kraken@live.ru>
2022-08-31 09:02:29 +0300

    temp

--- path.zsh
+++ path.zsh
@@ -2,3 +2,5 @@ PATH=$PATH:$HOME/bin
 PATH=$PATH:$HOME/go/bin
 export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig"
 export PATH="/opt/homebrew/opt/node@16/bin:$PATH"
+export PATH="/opt/homebrew/opt/node@16/bin:$PATH"
+export GOKU_EDN_CONFIG_FILE="$HOME/.dotfiles/karabiner.edn"
